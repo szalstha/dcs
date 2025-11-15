470bac9f47e3da8d9104b114ae455108cc7b6aff
szalstha <sujalshresthaf5@gmail.com>
2025-11-15 15:42:00 +0545

    Add files via upload

---
+++ D12.CPP
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int num, i = 1;
+    cout << "Enter a number: ";
+    cin >> num;
+
+    do {
+        cout << num << " x " << i << " = " << num * i << endl;
+        i++;
+    } while (i <= 10);
+
+    return 0;
+}
